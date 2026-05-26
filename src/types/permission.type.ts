84c83f8a8f6060e87ef1e76a087ba59361b4374f
Namdz18years <buicongnam@gmail.com>
2026-05-27 01:36:09 +0700

    update ui

--- src/types/permission.type.ts
+++ src/types/permission.type.ts
@@ -147,6 +147,9 @@ export const rolePermissions: RolePermissions = {
     VIEW.fines,
     VIEW.books,
     VIEW.purchases,
+    ...PERMISSIONS.borrows,
+    ...PERMISSIONS.extra,
+    ...PERMISSIONS.goods_receipts,
   ],
 
   user: []
@@ -156,7 +159,7 @@ export const isAdminRoute = (pathname: string) => {
   return menuPermissions.some((group) =>
     group.items.some((item) => {
       if (pathname === item.path) return true;
-      
+
       return pathname.startsWith(`${item.path}/`);
     })
   );
